677682f2fe49e6a090fc405b50683982ca26213c
DerZade <DerZade@gmail.com>
2018-12-18 00:38:41 +0100

    Fix styling of landing-page

--- docs/.vuepress/components/grad-landing-page.vue
+++ docs/.vuepress/components/grad-landing-page.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="grad-landing-page">
-      <h1>HIER SUCHEN:</h1>
-      <SearchBox/>
+      <h1 class="grad-landing-page__heading">Gruppe Adler Wiki</h1>
+      <div class="grad-landing-page__input-wrapper">
+        <img class="grad-landing-page__icon" src="https://forum.gruppe-adler.de/assets/uploads/system/site-logo.png" />
+        <SearchBox/>
+      </div>
   </div>
 </template>
 
@@ -12,3 +15,24 @@ export default {
   components: { SearchBox }
 }
 </script>
+
+<style lang="css" scoped>
+.grad-landing-page {
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+
+  align-self: center;
+}
+
+.grad-landing-page__input-wrapper {
+  display: flex;
+}
+
+.grad-landing-page__icon {
+  height: 2rem;
+  margin-right: 0.5rem;
+}
+
+
+</style>
--- docs/README.md
+++ docs/README.md
@@ -1,1 +1,6 @@
+---
+sidebar: false
+pageClass: grad-landing-page
+---
+
 <grad-landing-page/>
