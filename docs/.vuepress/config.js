33bb8252e6357f549cffcbf4ffa9212765b77d65
DerZade <DerZade@gmail.com>
2018-12-19 22:34:54 +0100

    Added Git Repo and Edit Links

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,6 +2,10 @@ module.exports = {
     title: 'Gruppe Adler Wiki',
     description: 'Gruppe Adler Wiki - Hier findest du alles von Organisationssturkur bis zu Taktikgelaber.',
     themeConfig: {
-        sidebar: 'auto'
+        sidebar: 'auto',
+        repo: 'gruppe-adler/vuepress-wiki',
+        editLinks: true,
+        docsDir: 'docs',
+        editLinkText: 'Seite auf GitHub bearbeiten'
     }
 }
