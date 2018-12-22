68a548a326a56483c0fd373fad16e98e9b60401b
DerZade <DerZade@gmail.com>
2018-12-23 00:36:29 +0100

    Fixed index layout

--- docs/.vuepress/theme/global-components/grad-index.vue
+++ docs/.vuepress/theme/global-components/grad-index.vue
@@ -1,12 +1,12 @@
 <template>
     <div>
-        <span v-for="page in $site.pages">
-            <a :href="page.regularPath">{{page.title}}</a>
+        <div v-for="page in $site.pages" :key="page.key" v-if="page.regularPath != '/'">
+            <h3><a :href="page.regularPath">{{page.title}}</a></h3>
             <p v-for="header in page.headers" :key="header.key">
-                <span v-for="i in (header.level - 1)" class="spacer" />
+                <span v-for="i in (header.level -1)" class="spacer" />
                 <a :href="page.regularPath+'#'+header.slug">{{header.title}}</a>
             </p>
-        </span>
+        </div>
     </div>
 </template>
 
