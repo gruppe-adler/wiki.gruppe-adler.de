c63fbd1531ae94c77be10020c6f373bf89fe0e47
DerZade <DerZade@gmail.com>
2018-12-26 15:57:44 +0100

    Fancy sidebar and nav bar links

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+var fs = require('fs');
 
 module.exports = {
     title: 'Gruppe Adler Wiki',
@@ -15,7 +16,6 @@ module.exports = {
     },
     theme: 'gruppe-adler-theme',
     themeConfig: {
-        sidebar: 'auto',
         repo: 'gruppe-adler/vuepress-wiki',
         editLinks: true,
         docsDir: 'docs',
@@ -25,14 +25,29 @@ module.exports = {
                 selectText: 'Sprachen',
                 editLinkText: 'Diese Seite auf GitHub bearbeiten',
                 lastUpdated: 'Als letztes geupdatet',
+                nav: [
+                    { text: 'Missionen', link: '/de/missionen/' },
+                    { text: 'Mods', link: '/de/mods/' },
+                    { text: 'Organisatorisches', link: '/de/organisatorisches/' },
+                    { text: 'Taktik', link: '/de/taktik/' },
+                    { text: 'Technik', link: '/de/technik/' }
+                ],
+                sidebar: {
+                    '/de/missionen/': getSidebarItems('Missionen', '/de/missionen'),
+                    '/de/mods/': getSidebarItems('Mods', '/de/mods'),
+                    '/de/organisatorisches/': getSidebarItems('Organisatorisches', '/de/organisatorisches'),
+                    '/de/taktik/': getSidebarItems('Taktik', '/de/taktik'),
+                    '/de/technik/': getSidebarItems('Technik', '/de/technik')
+                  }
             },
             '/en/': {
                 label: 'English',
                 selectText: 'Languages',
                 editLinkText: 'Edit this page on GitHub',
                 lastUpdated: 'Last Updated' ,
+                nav: []
             }
-        }
+        },
     },
     configureWebpack: {
         resolve: {
@@ -42,4 +57,31 @@ module.exports = {
         }
     },
     plugins: ['@vuepress/medium-zoom'] 
+}
+
+function getSidebarItems(name, relativePath, intro) {
+
+    // get all markdown files
+    let files = fs.readdirSync(path.resolve(__dirname, `..${relativePath}`)).filter(f => f.match(/\.md$/i));
+    
+    // add readme to front
+    let i = files.findIndex(f => f.match(/README\.md/i));
+    if (i > -1) {
+        files.splice(i, 1);
+        files.unshift('README.md')
+    }
+
+    let sites = files.map(f => {
+        if (f == 'README.md') return ['', intro || 'Einleitung'];
+
+        return f.slice(0, -3); // remove .md extension 
+    });
+
+    return [
+        {
+            title: name,
+            collapsable: false,
+            children: sites
+        }
+    ]
 }
