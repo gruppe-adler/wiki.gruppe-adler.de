582e46ef0175512862e26ee02d44c37d40ae184e
DerZade <DerZade@gmail.com>
2019-01-14 02:14:45 +0100

    Pleeease work...

--- travis-deploy.sh
+++ travis-deploy.sh
@@ -3,9 +3,9 @@ set -e
 
 # setup ssh-agent and provide the GitHub deploy key
 eval "$(ssh-agent -s)"
-openssl aes-256-cbc -K $encrypted_d84084d998cf_key -iv $encrypted_d84084d998cf_iv -in wiki.gruppe-adler.de-deploy.enc -out wiki.gruppe-adler.de-deploy -d
-chmod 600 wiki.gruppe-adler.de-deploy
-ssh-add wiki.gruppe-adler.de-deploy
+openssl aes-256-cbc -K $encrypted_d84084d998cf_key -iv $encrypted_d84084d998cf_iv -in travis-gh-pages-deploy.enc -out travis-gh-pages-deploy -d
+chmod 600 travis-gh-pages-deploy
+ssh-add travis-gh-pages-deploy
 
 # commit the assets in dist/ to the gh-pages branch and push to GitHub using SSH
 ./node_modules/.bin/gh-pages -d dist/ -b gh-pages -r git@github.com:${TRAVIS_REPO_SLUG}.git
