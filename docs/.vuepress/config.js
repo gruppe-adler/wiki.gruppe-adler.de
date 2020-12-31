8f0c8f652f5ca81659af11e8281de3eb55af6a62
DerZade <DerZade@gmail.com>
2020-12-31 20:49:40 +0100

    Added meta viewport

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -16,7 +16,8 @@ module.exports = {
         },
     },
     head: [
-        ['link', { rel: 'icon', href: '/favicon.ico' }]
+        ['link', { rel: 'icon', href: '/favicon.ico' }],
+        ['meta', { name: 'viewport', content: 'width=device-width, initial-scale=1' }],
     ],
     themeConfig: {
         repo: 'gruppe-adler/wiki.gruppe-adler.de',
