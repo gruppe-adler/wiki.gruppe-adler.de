8edbc356d1c5f58b368e6814235f3337b7db228d
DerZade <DerZade@users.noreply.github.com>
2023-10-19 12:14:25 +0000

    deploy: 5159e35dd8ad53f423c144ffd7563ec6d97d487c

---
+++ assets/index.html.325fae11.js
@@ -0,0 +1,1 @@
+const e={key:"v-8daa1a0e",path:"/",title:"",lang:"de",frontmatter:{layout:"LandingPage"},excerpt:"",headers:[],filePathRelative:"README.md",git:{updatedTime:1697717624e3,contributors:[{name:"DerZade",email:"DerZade@gmail.com",commits:1}]}};export{e as data};
