4177dfebd73823f502334c9288c4f6831e351078
DerZade <DerZade@users.noreply.github.com>
2024-01-20 10:27:16 +0000

    deploy: 1a1ecfe0627a3f3006042435a6cf1e500c3b2258

--- assets/index.html.2994f75b.js
+++
@@ -1,1 +0,0 @@
-const e={key:"v-8daa1a0e",path:"/",title:"",lang:"de",frontmatter:{layout:"LandingPage"},excerpt:"",headers:[],filePathRelative:"README.md",git:{updatedTime:1702550174e3,contributors:[{name:"gelir95",email:"felix.ulrich95@googlemail.com",commits:1}]}};export{e as data};
