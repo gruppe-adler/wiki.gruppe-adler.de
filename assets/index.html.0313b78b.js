a0fef1765fa7a62de1ade465975715c074ed13bf
DerZade <DerZade@users.noreply.github.com>
2024-05-09 11:12:50 +0000

    deploy: 05780a412ef8a1ddf70cb5268102b0c9a00d5955

--- assets/index.html.0313b78b.js
+++
@@ -1,1 +0,0 @@
-const t={key:"v-0df885de",path:"/taktik/",title:"\u{1F9ED} Taktik",lang:"de",frontmatter:{},excerpt:"",headers:[],filePathRelative:"taktik/README.md",git:{updatedTime:1713986608e3,contributors:[{name:"nomisum",email:"nomisum@googlemail.com",commits:1}]}};export{t as data};
---
+++ assets/index.html.26e5e662.js
@@ -0,0 +1,1 @@
+const t={key:"v-504b8cae",path:"/infrastruktur/",title:"\u2699\uFE0F Infrastruktur",lang:"de",frontmatter:{},excerpt:"",headers:[],filePathRelative:"infrastruktur/README.md",git:{updatedTime:1715253126e3,contributors:[{name:"DerZade",email:"derzade@gmail.com",commits:1}]}};export{t as data};
